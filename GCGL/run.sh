058dad65662bb6c66c9cb7d79db851760331795c
Jianhao Guo <786017746@qq.com>
2024-02-27 12:55:55 +0800

    update

--- GCGL/run.sh
+++
@@ -1,8 +0,0 @@
-method=$1
-gpu=$2
-
-CUDA_VISIBLE_DEVICES=$gpu python train.py --dataset SIDER-tIL \
-       --method $method \
-       --backbone GCN \
-       --gpu 0 \
-       --clsIL False
--- scripts/cora.sh
+++ scripts/cora.sh
@@ -4,17 +4,17 @@ gpu=$2
 
 #0,0.25,0.5,0.75,1
 # main exp
-# CUDA_VISIBLE_DEVICES=$gpu python train.py --dataset CoraFull-CL \
-#        --method my \
-#        --backbone MLP \
-#        --gpu 0 \
-#        --ILmode taskIL \
-#        --inter-task-edges False \
-#        --minibatch False \
-#        --n_cls_per_task 5 \
-#        --repeats 5 \
-#        --overwrite_result False \
-#        --my_args " 'diversity_ratio': [0.1]; 'sample_budget': [1000]; 'random_sample': False" 
+CUDA_VISIBLE_DEVICES=$gpu python train.py --dataset CoraFull-CL \
+       --method my \
+       --backbone MLP \
+       --gpu 0 \
+       --ILmode taskIL \
+       --inter-task-edges False \
+       --minibatch False \
+       --n_cls_per_task 5 \
+       --repeats 5 \
+       --overwrite_result False \
+       --my_args " 'diversity_ratio': [0.1]; 'sample_budget': [1000]; 'random_sample': False" 
 
 # ablation:-mlp,+gcn
 # CUDA_VISIBLE_DEVICES=$gpu python train.py --dataset CoraFull-CL \
